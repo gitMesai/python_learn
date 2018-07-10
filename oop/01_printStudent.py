05682dafb6506140730cb0feb62a0aaaaa4a6ec5
gitMesai <gitMesai@163.com>
2018-07-10 11:02:46 +0800

    1

--- oop/01_printStudent.py
+++ oop/01_printStudent.py
@@ -1,1 +1,9 @@
 class Student(object):
+    def __init__(self,name,score):
+        self.name = name;
+        self.score = score;
+    def print_score(self):
+        print('%s : %s' %(self.name,self.score));
+
+bart = Student('liwei',91)
+bart.print_score()
